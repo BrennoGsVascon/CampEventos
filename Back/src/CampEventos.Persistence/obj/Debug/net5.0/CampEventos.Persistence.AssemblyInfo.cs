0548fef5d39ffd60c9fd7c89864c1646ffe0bb68
BrennoGsVascon <br4nnogsvascon@gmail.com>
2026-05-14 19:08:50 -0300

    Atualizando Banco de Dados

--- Back/src/CampEventos.Persistence/obj/Debug/net5.0/CampEventos.Persistence.AssemblyInfo.cs
+++ Back/src/CampEventos.Persistence/obj/Debug/net5.0/CampEventos.Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampEventos.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09973dcb99c4c9900d72bf0a45190cf520d88511")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampEventos.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampEventos.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
