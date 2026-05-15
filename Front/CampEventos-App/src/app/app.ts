66fbf5618b63930cb6a9c5d4af0a393e838b4a76
Brenno Vasconcellos <br4nnogsvascon@gmail.com>
2026-05-15 15:41:03 -0300

    Finalizando section 7 e iniciando section 8

--- Front/CampEventos-App/src/app/app.ts
+++ Front/CampEventos-App/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./shared/nav/nav.component";
 
