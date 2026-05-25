72eb038fc26b4a9eea365e14e422007db5f98ace
BrennoGsVascon <br4nnogsvascon@gmail.com>
2026-05-25 05:38:49 -0300

    Processo de autenticação iniciado

---
+++ Back/src/CampEventos.Domain/Identity/Role.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampEventos.Domain.Identity
+{
+    public class Role
+    {
+        
+    }
+}
